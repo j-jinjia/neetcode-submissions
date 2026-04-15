0f517d12e874c304033c518128b25fe5e45b2733
Jiazheng Jin Jia <98120734+j-jinjia@users.noreply.github.com>
2026-04-15 17:33:35 +0100

    Add: redistribute-characters-to-make-all-strings-equal - submission-0

---
+++ Data Structures & Algorithms/redistribute-characters-to-make-all-strings-equal/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean makeEqual(String[] words) {
+        Map<Character, Integer> map =  new HashMap<>();
+
+        for(String word: words) {
+            for (char c : word.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+
+        }
+
+        for (int value : map.values()) {
+            if (value % words.length != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
